5c62c99319ee3f3f3daaff6aff08d56ec47b44ee
tomFoolery42 <>
2025-09-01 09:29:01 -0500

    fixed memory leaks. Tests work

---
+++ src/database.zig
@@ -0,0 +1,180 @@
+const schema = @import("schema.zig");
+
+const ai = @import("zig_ai");
+const Allocator = std.mem.Allocator;
+const fr = @import("fridge");
+const std = @import("std");
+const Sha256 = std.crypto.hash.sha2.Sha256;
+
+const ID = schema.ID;
+const BUF_SIZE = 16;
+const Self = @This();
+const String = schema.String;
+
+const description_system = "You are the worlds greatest meme smith. Your usage of words to describe images are unmatched. Please apply your abilities to the following image. Try to keep it under 2 paragraphs.";
+const model = "gemma3:4b";
+const tags_system = "You are the worlds greatest meme smith. You generate tags to give breif descriptions of images. Give a list of tags that would apply to the following image. Your response should be in the form of a comma separated list.";
+
+alloc: Allocator,
+client: ai.Client,
+database_root: String,
+db: fr.Session,
+
+pub fn init(alloc: Allocator, ollama_url: String, database_root: String, metadata: [:0]const u8) !*Self {
+    var self = try alloc.create(Self);
+    self.* = .{
+        .alloc = alloc,
+        .client = try ai.Client.init(alloc, ollama_url, "ollama", null),
+        .database_root = database_root,
+        .db = try fr.Session.open(fr.SQLite3, alloc, .{ .filename = metadata }),
+    };
+    
+    self.db.conn.execAll(
+        \\CREATE TABLE Image (
+        \\  id INTEGER PRIMARY KEY,
+        \\  description TEXT NOT NULL,
+        \\  filename TEXT NOT NULL,
+        \\  tags TEXT NOT NULL
+        \\);
+    ) catch {std.log.info("table already exists", .{});};
+
+    // initial integrity check. If a file is missing, report and remove
+    for (try self.db.query(schema.Image).findAll()) |image| {
+        if (exists(image.filename) == false) {
+            std.log.warn("missing {s}. Removing from database", .{image.filename});
+            try self.delete(image.id);
+        }
+    }
+
+    // add any image that is just in the database folder into the metadata sql
+    var dir = try std.fs.cwd().openDir("database", .{.iterate = true});
+    var walker = try dir.walk(alloc);
+    defer walker.deinit();
+
+    while (try walker.next()) |next| {
+        const ext = std.fs.path.extension(next.basename);
+        if (std.mem.eql(u8, ext, ".jpg")) {
+            const path = try std.fmt.allocPrint(alloc, "{s}/{s}", .{"database", next.basename});
+            defer alloc.free(path);
+            _ = try self.insert(path);
+        }
+    }
+
+    return self;
+}
+
+pub fn deinit(self: *Self) void {
+    self.client.deinit();
+    self.db.deinit();
+    self.alloc.destroy(self);
+}
+
+pub fn delete(self: *Self, id: ID) !void {
+    if (self.db.query(schema.Image).find(id)) |found| {
+        if (found) |image| {
+            _ = try std.process.Child.run(.{.allocator = self.alloc, .argv = &.{"rm ", image.filename}});
+            _ = try self.db.delete(schema.Image, id);
+        }
+    }
+    else |_| {
+        std.log.info("no image with id {d}", .{id});
+    }
+}
+
+fn descriptionGenerate(self: *Self, filename: String) !String {
+    const file = try std.fs.cwd().openFile(filename, .{});
+    const messages: []const ai.Message = &.{
+        .system(description_system),
+        try .image(self.alloc, "Please write a description of this image", file),
+    };
+    defer {
+        for (messages) |next| {
+            next.deinit();
+        }
+    }
+
+    const response = try self.client.chat(.{
+        .model = model,
+        .messages = messages,
+        .max_tokens = 10000,
+        .temperature = 0.7
+    }, false);
+    defer response.deinit();
+
+    return self.alloc.dupe(u8, response.value.choices[0].message.content);
+}
+
+fn exists(filename: String) bool {
+    if (std.fs.cwd().openFile(filename, .{})) |file| {
+        defer file.close();
+        return true;
+    }
+    else |_| {return false;}
+}
+
+pub fn findAll(self: *Self) ![]const schema.Image {
+    return try self.db.query(schema.Image).findAll();
+}
+
+pub fn insert(self: *Self, og_path: String) !ID {
+    std.log.info("inserting {s}", .{og_path});
+    const file = try std.fs.cwd().openFile(og_path, .{});
+    defer file.close();
+    const extension = std.fs.path.extension(og_path);
+    const digest = try sha256Digest(file);
+    const hashed_name = try std.fmt.allocPrint(self.alloc, "{s}/{s}{s}", .{self.database_root, std.fmt.fmtSliceHexLower(&digest), extension});
+    defer self.alloc.free(hashed_name);
+    try std.fs.cwd().copyFile(og_path, std.fs.cwd(), hashed_name, .{});
+    const description = try self.descriptionGenerate(hashed_name);
+    defer self.alloc.free(description);
+    const tags = try self.tagsGenerate(hashed_name);
+    defer self.alloc.free(tags);
+
+    return self.db.insert(schema.Image, .{
+        .filename = hashed_name,
+        .description = description,
+        .tags = tags,
+    });
+}
+
+pub fn like(self: *Self, query: String, search: String) ![]const schema.Image {
+    const request = self.db.query(schema.Image).whereRaw(query, search);
+    return request.findAll();
+}
+
+fn sha256Digest(file: std.fs.File) ![Sha256.digest_length]u8 {
+    var sha256 = Sha256.init(.{});
+    const rdr = file.reader();
+
+    var buf: [BUF_SIZE]u8 = undefined;
+    var n = try rdr.read(&buf);
+    while (n != 0) {
+        sha256.update(buf[0..n]);
+        n = try rdr.read(&buf);
+    }
+
+    return sha256.finalResult();
+}
+
+fn tagsGenerate(self: *Self, filename: String) !String {
+    const file = try std.fs.cwd().openFile(filename, .{});
+    const messages: []const ai.Message = &.{
+        .system(tags_system),
+        try .image(self.alloc, "Please write a description of this image", file),
+    };
+    defer {
+        for (messages) |next| {
+            next.deinit();
+        }
+    }
+
+    const response = try self.client.chat(.{
+        .model = model,
+        .messages = messages,
+        .max_tokens = 10000,
+        .temperature = 0.7
+    }, false);
+    defer response.deinit();
+
+    return self.alloc.dupe(u8, response.value.choices[0].message.content);
+}
--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,4 @@
+const Database = @import("database.zig");
 const event = @import("events.zig");
 const queues = @import("message_queue.zig");
 const schema = @import("schema.zig");
@@ -10,7 +11,7 @@ const std = @import("std");
 const Sha256 = std.crypto.hash.sha2.Sha256;
 
 
-const BUF_SIZE = 16;
+pub const log_level: std.log.Level = .info;
 const EventQueue = queues.MessageQueue(event.Event, 100);
 const ID = schema.ID;
 const Socket = socket(event.Event);
@@ -30,91 +31,7 @@ fn socketHandle() void {
     }
 }
 
-fn databaseInit(alloc: Allocator, db: *fr.Session, client: *ai.Client) !void {
-    db.conn.execAll(
-        \\CREATE TABLE Image (
-        \\  id INTEGER PRIMARY KEY,
-        \\  description TEXT NOT NULL,
-        \\  filename TEXT NOT NULL,
-        \\  tags TEXT NOT NULL
-        \\);
-    ) catch {std.log.info("table already exists", .{});};
-
-    // initial integrity check. If a file is missing, report and remove
-    for (try db.query(schema.Image).findAll()) |image| {
-        if (exists(image.filename) == false) {
-            std.log.warn("missing {s}. Removing from database", .{image.filename});
-            try db.delete(schema.Image, image.id);
-        }
-    }
-
-    // add any image that is just in the database folder into the metadata sql
-    var dir = try std.fs.cwd().openDir("database", .{.iterate = true});
-    var walker = try dir.walk(alloc);
-    defer walker.deinit();
-
-    while (try walker.next()) |next| {
-        const ext = std.fs.path.extension(next.basename);
-        if (std.mem.eql(u8, ext, ".jpg")) {
-            const path = try std.fmt.allocPrint(alloc, "{s}/{s}", .{"database", next.basename});
-            defer alloc.free(path);
-            _ = try insert(alloc, db, client, path);
-        }
-    }
-}
-
-fn description_generate(alloc: Allocator, client: *ai.Client, filename: String) !String {
-    const file = try std.fs.cwd().openFile(filename, .{});
-    const messages: []const ai.Message = &.{
-        .system("You are the worlds greatest radio host. Your usage of words to describe images are unmatched. Please apply your abilities to the following image. Try to keep it under 2 paragraphs."),
-        try .image(alloc, "Please write a description of this image", file),
-    };
-    defer {
-        for (messages) |next| {
-            next.deinit();
-        }
-    }
-
-    const response = try client.chat(.{
-        .model = "gemma3:4b",
-        .messages = messages,
-        .max_tokens = 10000,
-        .temperature = 0.7
-    }, false);
-    defer response.deinit();
-
-    return alloc.dupe(u8, response.value.choices[0].message.content);
-}
-
-fn exists(filename: String) bool {
-    if (std.fs.cwd().openFile(filename, .{})) |file| {
-        defer file.close();
-        return true;
-    }
-    else |_| {return false;}
-}
-
-fn insert(alloc: Allocator, db: *fr.Session, client: *ai.Client, og_path: String) !ID {
-    std.log.info("inserting {s}", .{og_path});
-    const file = try std.fs.cwd().openFile(og_path, .{});
-    defer file.close();
-    const extension = std.fs.path.extension(og_path);
-    const digest = try sha256_digest(file);
-    const hashed_name = try std.fmt.allocPrint(alloc, "database/{s}{s}", .{std.fmt.fmtSliceHexLower(&digest), extension});
-    defer alloc.free(hashed_name);
-    try std.fs.cwd().copyFile(og_path, std.fs.cwd(), hashed_name, .{});
-    const description = try description_generate(alloc, client, hashed_name);
-    defer alloc.free(description);
-    const tags = try tags_generate(alloc, client, hashed_name);
-    defer alloc.free(tags);
-    return db.insert(schema.Image, .{
-        .filename = hashed_name,
-        .description = description,
-        .tags = tags,
-    });
-}
-
-fn open_config(alloc: std.mem.Allocator, config_file: []const u8) !std.json.Parsed(Config) {
+fn openConfig(alloc: std.mem.Allocator, config_file: []const u8) !std.json.Parsed(Config) {
     var file = try std.fs.cwd().openFile(config_file, .{});
     defer file.close();
 
@@ -123,58 +40,15 @@ fn open_config(alloc: std.mem.Allocator, config_file: []const u8) !std.json.Pars
     return try std.json.parseFromSlice(Config, alloc, json, .{.allocate = .alloc_always, .ignore_unknown_fields = true});
 }
 
-fn sha256_digest(file: std.fs.File) ![Sha256.digest_length]u8 {
-    var sha256 = Sha256.init(.{});
-    const rdr = file.reader();
-
-    var buf: [BUF_SIZE]u8 = undefined;
-    var n = try rdr.read(&buf);
-    while (n != 0) {
-        sha256.update(buf[0..n]);
-        n = try rdr.read(&buf);
-    }
-
-    return sha256.finalResult();
-}
-
-fn tags_generate(alloc: Allocator, client: *ai.Client, filename: String) !String {
-    const file = try std.fs.cwd().openFile(filename, .{});
-    const messages: []const ai.Message = &.{
-        .system("You are the worlds greatest tag generator. You generate tags to give breif descriptions of images. Give a list of tags that would apply to the following image. Your response should be in the form of a comma separated list."),
-        try .image(alloc, "Please write a description of this image", file),
-    };
-    defer {
-        for (messages) |next| {
-            next.deinit();
-        }
-    }
-
-    const response = try client.chat(.{
-        .model = "gemma3:4b",
-        .messages = messages,
-        .max_tokens = 10000,
-        .temperature = 0.7
-    }, false);
-    defer response.deinit();
-
-    return alloc.dupe(u8, response.value.choices[0].message.content);
-}
-
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer std.debug.assert(gpa.deinit() == .ok);
     const alloc = gpa.allocator();
 
-    const config = try open_config(alloc, "config.json");
+    const config = try openConfig(alloc, "config.json");
     defer config.deinit();
-    var client = try ai.Client.init(alloc, config.value.ai_url, "ollama", null);
-    defer client.deinit();
-
-    var db = try fr.Session.open(fr.SQLite3, alloc, .{ .filename = config.value.metadata });
-    defer db.deinit();
-    databaseInit(alloc, &db, &client) catch |err| {
-        std.log.debug("Figure out if there is a way to verify db being created other than catch.\n{s}", .{std.json.fmt(err, .{})});
-    };
+    var database = try Database.init(alloc, config.value.ai_url, config.value.database, config.value.metadata);
+    defer database.deinit();
     var queue = EventQueue.init();
     defer queue.deinit();
 
@@ -185,15 +59,7 @@ pub fn main() !void {
                 .Bail   => running = false,
                 .Delete => |to_delete| {
                     for (to_delete) |id| {
-                        if (db.query(schema.Image).find(id)) |found| {
-                            if (found) |image| {
-                                _ = try std.process.Child.run(.{.allocator = alloc, .argv = &.{"rm ", image.filename}});
-                                _ = try db.delete(schema.Image, id);
-                            }
-                        }
-                        else |_| {
-                            std.log.info("no image with id {d}", .{id});
-                        }
+                        try database.delete(id);
                     }
                 },
 //                .Fetch => |filter| {
@@ -201,17 +67,17 @@ pub fn main() !void {
 //                        .description    => |desc| ret: {
 //                            const filtering = std.fmt.allocPrint(alloc, "%{s}%", .{desc});
 //                            defer alloc.free(filtering);
-//                            break :ret db.query(schema.Image).whereRaw("description LIKE", desc);
+//                            break :ret database.query(schema.Image).whereRaw("description LIKE", desc);
 //                        },
 //                        .filename       => |name| ret: {
 //                            const filtering = std.fmt.allocPrint(alloc, "%{s}%", .{name});
 //                            defer alloc.free(filtering);
-//                            break :ret db.query(schema.Image).whereRaw("filename LIKE", name);
+//                            break :ret database.query(schema.Image).whereRaw("filename LIKE", name);
 //                        },
 //                        .tags           => |tags| ret: {
 //                            const filtering = std.fmt.allocPrint(alloc, "%{s}%", .{tags});
 //                            defer alloc.free(filtering);
-//                            break :ret db.query(schema.Image).whereRaw("tags LIKE", tags);
+//                            break :ret database.query(schema.Image).whereRaw("tags LIKE", tags);
 //                        },
 //                    };
 //
@@ -219,8 +85,8 @@ pub fn main() !void {
 //                },
                 .Insert => |to_insert| {
                     for (to_insert) |next| {
-                        _ = try db.insert(schema.Image, next);
-                        //_ = try insert(alloc, &db, &client, next);
+                        _ = try database.insert(next);
+                        //_ = try insert(alloc, &database, &client, next);
                     }
                 },
                 else => {},
@@ -230,32 +96,44 @@ pub fn main() !void {
     }
 }
 
-test "search test" {
+test "init" {
+    std.testing.log_level = std.log.Level.info;
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer std.debug.assert(gpa.deinit() == .ok);
     const alloc = gpa.allocator();
 
-    var db = try fr.Session.open(fr.SQLite3, alloc, .{ .filename = "database/metadata.sqlite" });
-    defer db.deinit();
-    databaseInit(&db) catch {
-        std.log.debug("Figure out if there is a way to verify db being created other than catch.", .{});
-    };
+    var database = try Database.init(alloc, "https://test.url/v1", "database", "database/test.sqlite");
+    defer database.deinit();
+
+    _ = try database.insert("pictures/meme/1047646.jpg");
+    _ = try database.insert("pictures/meme/134664f.jpg");
+    _ = try database.insert("pictures/meme/2323e70.jpg");
+    _ = try database.insert("pictures/meme/3361566.jpg");
+    _ = try database.insert("pictures/meme/789bd0c.jpg");
+
+    std.log.info("inserted images", .{});
+    for (try database.findAll()) |image| {
+        std.log.info("Image: {s}", .{std.json.fmt(image, .{})});
+    }
+}
+
+test "search" {
+    std.testing.log_level = std.log.Level.info;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const alloc = gpa.allocator();
+
+    var database = try Database.init(alloc, "https://test.url/v1", "database", "database/test.sqlite");
+    defer database.deinit();
     var queue = EventQueue.init();
     defer queue.deinit();
 
-    var id = try db.insert(schema.Image, .{.filename = "test.jpg", .description = "test image with stuff", .tags="great,another,og"});
-    std.log.info("id: {}", .{id});
-    id = try db.insert(schema.Image, .{.filename = "another.jpg", .description = "another image", .tags="awesome,another,better"});
-    std.log.info("id: {}", .{id});
-    id = try db.insert(schema.Image, .{.filename = "bigboy.jpg", .description = "big boy image", .tags="great,awesome,better"});
-    std.log.info("id: {}", .{id});
-
     std.log.info("basic search", .{});
-    for (try db.query(schema.Image).findAll()) |image| {
+    for (try database.findAll()) |image| {
         std.log.info("Image: {}", .{std.json.fmt(image, .{})});
     }
 
     std.log.info("col search", .{});
-    const request = db.query(schema.Image).whereRaw("tags LIKE ?", "%better%");
-    std.log.info("search: {s}\t found: {s}", .{"better", std.json.fmt(try request.findAll(), .{})});
+    const results = try database.like("tags LIKE", "%meme%");
+    std.log.info("search: {s}\t found: {s}", .{"better", std.json.fmt(results, .{})});
 }
